6149812f2a21b192c0507d4dd7c7f7b284425af0
Paul Hwang <mothcar@gmail.com>
2018-07-06 21:56:59 +0900

    Test : computed get / set

--- src/views/SecureLogin.vue
+++ src/views/SecureLogin.vue
@@ -1,9 +1,31 @@
 <template>
-    <div id="login">
-        <h1>Login</h1>
-        <input type="text" name="username" v-model="input.username" placeholder="Username" />
-        <input type="password" name="password" v-model="input.password" placeholder="Password" />
-        <button type="button" v-on:click="login()">Login</button>
+    <div>
+
+
+        <div class="login_box">
+            <h1>See what’s happening in the world right now</h1>
+        </div>
+        <div class="login_box">
+            <h1>Log in</h1>
+            <input type="text" name="username" v-model="input.username" placeholder="Username" />
+            <input type="password" name="password" v-model="input.password" placeholder="Password" />
+            <button type="button" v-on:click="login">Login</button>
+        </div>
+        <div class="login_box">
+            <h1>password</h1>
+        </div>
+        <div class="login_box">
+            <h1>forget password</h1>
+        </div>
+        <div class="login_box">
+            <button @click="changeNameSetter">{{ money }}</button>
+        </div>
+        <div class="login_box">
+            <h3>create accoutn</h3>
+        </div>
+        <div class="login_box">
+            <h1>Sign Up</h1>
+        </div>
     </div>
 </template>
 
@@ -15,10 +37,37 @@
                 input: {
                     username: "",
                     password: ""
-                }
+                },
+                firstName: 'Bo',
+                lastName: 'Andersen',
+                money: 0
             }
         },
+
+        computed: {
+            updatedMoney: {
+        		get: function() {
+                    // alert("Assembling full name...");
+	                //    return this.money -= 50;
+
+        		},
+        		set: function(newValue) {
+                    // alert("Setting new name: " + newValue);
+                	// var parts = newValue.split(' ');
+                	// this.firstName = parts[0];
+                	// this.lastName = parts[parts.length - 1];
+                    this.money = newValue
+
+        		}
+        	}
+        },
+
         methods: {
+            changeNameSetter: function() {
+                this.updatedMoney = this.money+500;
+
+	        },
+
             login() {
 
                 // http://119.205.233.249:3000/v1/auth/signin?email=mo@naver.com&password=111111
@@ -65,12 +114,12 @@
 </script>
 
 <style scoped>
-    #login {
+    .login_box {
         width: 500px;
         border: 1px solid #CCCCCC;
         background-color: #FFFFFF;
         margin: auto;
-        margin-top: 200px;
+        margin-top: 20px;
         padding: 20px;
     }
 </style>
