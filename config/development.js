cea068a9739414e39287fbfbf57cb67611c0df9e
Paul Hwang <mothcar@gmail.com>
2018-07-07 21:23:46 +0900

    Test: Gitignore

--- config/development.js
+++ config/development.js
@@ -2,7 +2,8 @@
 
 var p_config = {
   development: 'umanji dev',
-  production: 'umanji procution dev'
+  production: 'umanji procution dev',
+  secret: 'Do not commit this file'
   // staging: require('./staging.config')
 }
 
