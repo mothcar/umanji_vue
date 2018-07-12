e650c9dcfbf56dae79320647c8bdae88cbd71195
Paul Hwang <mothcar@gmail.com>
2018-07-12 17:19:15 +0900

    Done : profile Basic Layout

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,19 +1,21 @@
 <template>
-<div >
-  <div class="p_container">
-    <h1> This is profile</h1>
-  </div>
-
   <div class="p_container">
 
-    <div class="p_box">
-      header box
+    <div class="p_box p_header">
+      <h1> Profile - Paul Hwang</h1>
     </div>
-    <div class="p_box">
-      left box
+    <div class="p_box p_side">
+      <h2> menu </h2>
     </div>
-    <div class="p_box">
-      main box
+    <div class="p_box p_main">
+      <div class="p_main_container">
+        <h2 class="p_p">main - Possess</h2>
+        <h3 class="p_p">admin Level : 5</h3>
+        <h3 class="p_p">Money : 34,000</h3>
+        <h3 class="p_p">Building : 5</h3>
+        <h3 class="p_p">Blogs : 15</h3>
+
+      </div>
     </div>
 
     <div class="p_box">
@@ -24,28 +26,54 @@
 
 
 
-</div>
 </template>
 
 <style scoped>
+.p_p {
+  padding: 3px;
+
+}
+
 .p_container {
   background: #777;
   display: flex;
-  margin: 5px;
   flex-wrap: wrap;
   justify-content: center;
-  flex: 1 100px;
 }
 .p_box {
-  width: 100%;
-
-  background: red;
-  margin:10px;
   padding: 10px;
 }
 
-.p_box img {
+.p_header {
+  width : 100%;
+  background: #ccc;
+  flex-grow: 1;
+}
+
+.p_side {
+  min-width : 20%;
+  flex-grow: 1;
+  flex-shirk: 2;
+  background: #eee;
+}
+
+.p_main {
+  /* width : 70%; */
+  min-width: 380px;
+  flex-grow: 2;
+  flex-shirk: 1;
+  background: #ddd;
+}
+
+.p_main_container {
+  margin: 10px;
+  padding: 10px;
+  background: #fff;
+}
+
+img {
   width: 100%;
+  height: 100%;
 }
 </style>
 
